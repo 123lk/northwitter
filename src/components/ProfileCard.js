61466df38217acde4b56d83fe0b549629a1d56bf
laura <laura_kenny85@hotmail.com>
2017-05-21 18:26:08 +0100

    updated col settings to make site more responsive

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -43,6 +43,7 @@ class ProfileCard extends React.Component {
           </div>
         </div>
         <div className='profile-card-lower'>
+          <p className='text-center'>
           <div className='profile-tweets'>
             <p><b>TWEETS</b></p>
             <span className='profile-tweets-number'>{this.state.user_data.tweets}</span>
@@ -55,6 +56,7 @@ class ProfileCard extends React.Component {
             <p><b>FOLLOWERS</b></p>
             <span className='profile-followers-number'>{this.state.user_data.followers}</span>
           </div>
+          </p>
         </div>
       </div>
     );
