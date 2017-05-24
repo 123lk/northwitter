c07a8a71ccce9f7b4d0b18e2220d4e1fed02460c
laura <laura_kenny85@hotmail.com>
2017-05-24 20:32:25 +0100

    trends feed is now hidden on sm and xs screens

--- src/components/DashboardLeft.js
+++ src/components/DashboardLeft.js
@@ -3,11 +3,13 @@ import ProfileCard from './ProfileCard';
 import TrendsFeed from './TrendsFeed';
 
 class DashboardLeft extends React.Component {
-  render () {
+  render() {
     return (
       <div className='col-md-3'>
         <ProfileCard />
-        <TrendsFeed />
+        <div className='hidden-sm hidden-xs'>
+          <TrendsFeed />
+        </div>
       </div>
     );
   }
